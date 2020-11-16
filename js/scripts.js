193d412b6275a8ffc16af3ed8547bce4d232d765
Carley Jeffrey <carleyjeffrey88@gmail.com>
2020-11-16 11:53:16 +0000

    Inserted template HTML into the gallary div

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 fetch("https://randomuser.me/api/")
   .then(response => response.json())
   .then(data => console.log(data))
+
+const gallery = document.getElementById('gallery');
+
+const galleryHTML = `
+  <div class="card">
+      <div class="card-img-container">
+          <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+      </div>
+      <div class="card-info-container">
+          <h3 id="name" class="card-name cap">first last</h3>
+          <p class="card-text">email</p>
+          <p class="card-text cap">city, state</p>
+      </div>
+  </div>`
+
+gallery.insertAdjacentHTML('beforeEnd', galleryHTML);
